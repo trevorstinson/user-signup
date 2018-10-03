7b5ef6b9915779fe40388298a3e1a0515b6b13b9
Trevor Stinson <trevor@trevorstinson.com>
2018-10-03 09:12:55 -0500

    Set up username_error, password_error, and verify_error

--- main.py
+++ main.py
@@ -24,13 +24,33 @@ def index():
         password_error = ''
         verify_error = ''
 
+        # Check for verify_error first
+        # This allows other errors to override it so that
+        # user doesn't waste time matching these two fields
+        # before knowing the other password criteria
+        if validate.is_empty(verify):
+            password_error = "Please reenter your password"
+            verify_error = "Please verify your password"
+        if validate.does_not_match(password, verify):
+            password_error = "Please reenter password"
+            verify_error = "Verification must match"
+
+        if validate.is_out_of_range(username):
+            username_error = "Username must be 3-20 characters"
+        if validate.is_out_of_range(password):
+            password_error = "Password must be 3-20 characters"
+            verify_error = ''
+
+        if validate.contains_space(username):
+            username_error = "Username cannot contain spaces"
+        if validate.contains_space(password):
+            password_error = "Password cannot contain spaces"
+
+
         if validate.is_empty(username):
             username_error = "Please enter a username"
         if validate.is_empty(password):
             password_error = "Please enter a password"
-        if validate.is_empty(verify):
-            password_error = "Please enter and verify a password"
-            verify_error = "Please verify your password"
 
         if username_error or email_error or password_error or verify_error:
             return render_template('signup_form.html', title = "User Signup",
--- validate.py
+++ validate.py
@@ -5,7 +5,19 @@ def is_empty(user_input):
         return False
 
 def is_out_of_range(user_input):
-    if 2 < len(user_input) < 21:
+    if len(user_input) < 3 or len(user_input) > 20:
+        return True
+    else:
+        return False
+
+def contains_space(user_input):
+    if ' ' in user_input:
+        return True
+    else:
+        return False
+
+def does_not_match(user_input, user_verify):
+    if user_input != user_verify:
         return True
     else:
         return False
